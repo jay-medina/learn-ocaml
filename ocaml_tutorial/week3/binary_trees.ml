a5b2cefda53d9b2919becac00005f8ef08675a2d
Jose Medina <josemedina921@gmail.com>
2018-11-19 20:40:16 -0800

    finish binary trees

--- ocaml_tutorial/week3/binary_trees.ml
+++ ocaml_tutorial/week3/binary_trees.ml
@@ -13,9 +13,41 @@ type 'a bt =
   | Empty
   | Node of 'a bt * 'a * 'a bt ;;
 
+let max x y = if x > y then x else y
 
 let rec height t = 
-  "Replace this string with your implementation." ;;
+  match t with 
+  | Empty -> 0
+  | Node (l, _, r) -> 
+      let l_height = height l in 
+      let r_height = height r in 
+      (max l_height r_height) + 1
+                    
 
 let rec balanced t  = 
-  "Replace this string with your implementation." ;;
+  match t with
+  | Empty -> true
+  | Node (l, _, r) -> 
+      (height l) = (height r)
+
+
+(* tests *)
+let b_tree = Node(Empty, 4 , Node(Empty, 5, Empty));;
+
+let sample = (Node (Node (Node (Node (Node 
+  (Empty, 3, Empty), 4, Node (Empty, 1, Empty)), 0, Node (Node 
+                         (Empty, 2, Empty), 
+                         2, 
+                         Empty
+                       )
+                   ),
+                   -4, 
+                   Node (Empty, -3, Empty)
+                  ),
+    0,
+    Node (Empty, 2,
+     Node (Empty, -4, Node (Empty, 1, Node (Empty, -5, Empty))))))
+
+let ans = print_int (height b_tree)
+
+let isBalanced = print_bool (balanced b_tree)
