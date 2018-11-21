89848b128282aea619d06ef7e17c2416f65ac505
Jose Medina <josemedina921@gmail.com>
2018-11-20 21:47:26 -0800

    simplifying?

--- ocaml_tutorial/week3/advance_patterns.ml
+++ ocaml_tutorial/week3/advance_patterns.ml
@@ -13,25 +13,18 @@ Let's rewrite some pattern matching with advanced constructs.
 type e = EInt of int | EMul of e * e | EAdd of e * e
 
 let simplify = function
-  | EMul (EInt 1, e) -> e
-  | EMul (e, EInt 1) -> e
-  | EMul (EInt 0, e) -> EInt 0
-  | EMul (e, EInt 0) -> EInt 0
-  | EAdd (EInt 0, e) -> e
-  | EAdd (e, EInt 0) -> e
-  | e -> e
+  | EMul (EInt 1, e) | EMul(e, EInt 1) -> e
+  | EMul (EInt 0, e) | EMul (e, EInt 0) -> EInt 0
+  | EAdd (EInt 0, e) | EAdd (e, EInt 0) | e -> e
 
 let only_small_lists = function
-  | [] -> []
-  | [x] -> [x]
-  | [x;y] -> [x;y]
+  | ([] | [_] | [_;_]) as l -> l
   | _ -> []
 
 let rec no_consecutive_repetition = function
   | [] -> []
   | [x] -> [x]
-  | x :: y :: ys ->
-      if x = y then
+  | x :: y :: ys when x = y ->
         no_consecutive_repetition (y :: ys)
-      else
-        x :: (no_consecutive_repetition (y :: ys))
+  | x :: rest ->
+        x :: (no_consecutive_repetition rest)
