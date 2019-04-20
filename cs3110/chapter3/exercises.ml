1afc784bef7c9edaad7c87dc31742d46e2d8cfe3
Jose Medina <josemedina921@gmail.com>
2019-04-19 21:14:51 -0700

    finish ch3

--- cs3110/chapter3/exercises.ml
+++ cs3110/chapter3/exercises.ml
@@ -115,4 +115,141 @@ let powerset (li: int list) =
   in
     loop [[]] li
   
+  (* student *)
+type student = { first_name: string; last_name: string; gpa: float }
 
+let s: student = {
+  first_name = "Jose";
+  last_name = "Medina";
+  gpa = 3.4;
+}
+
+let getStudentsName (s: student) = s.first_name ^ " " ^ s.last_name
+
+let createStudent first last gpa = {
+  first_name = first; 
+  last_name = last;
+  gpa = gpa;
+}
+
+(* pokerecord *)
+type poketype = Normal | Fire | Water
+
+type pokemon = {
+  name: string;
+  hp: int;
+  ptype: poketype;
+}
+
+let charizard: pokemon = {
+  name= "charizard";
+  hp= 78;
+  ptype= Fire;
+}
+
+let metapod: pokemon = {
+  name= "metapod";
+  hp=50;
+  ptype=Normal;
+}
+
+(* safe hd and tl *)
+let safe_hd li = 
+  match li with 
+  | [] -> None
+  | x::_ -> Some x
+
+let safe_tl li = 
+  match li with 
+  | [] -> None
+  | _::xs -> Some xs
+
+(* pokefun  *)
+let rec max_hp pokemon_list = 
+  let rec findBiggest biggest rest = 
+    match rest with 
+    | [] -> biggest
+    | p::ps -> if biggest.hp > p.hp then (findBiggest biggest ps)
+               else (findBiggest p ps)
+  in
+    match pokemon_list with 
+    | [] -> None
+    | p::ps -> Some (findBiggest p ps)
+
+(* date before *)
+(* year * month * day *)
+type date = int * int * int
+
+let is_before (date1: date) (date2: date) = 
+ let (y1, m1, d1) = date1 in
+ let (y2, m2, d2) = date2 in
+ if (y1 < y2) then true
+ else if (y1 = y2 && m1 < m2) then true
+ else if (y1 = y2 && m1 = m2) then d1 = d2
+ else false
+
+(* earliest date *)
+let earliest (date_list: date list) = 
+  let rec loop selected rest = 
+    match rest with 
+    | [] -> selected
+    | d::ds -> if (is_before selected d) then (loop selected ds)
+               else (loop d ds)
+  in
+  match date_list with
+  | [] -> None
+  | d::ds -> Some (loop d ds)
+
+(* cards *)
+type suit = Hearts | Diamonds | Spades | Clubs
+type rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace
+
+type card = { suit: suit; rank: rank }
+
+let c1: card = { suit = Clubs; rank = Ace }
+let c1: card = { suit = Hearts; rank = Queen }
+let c1: card = { suit = Diamonds; rank = Two }
+let c1: card = { suit = Spades; rank = Seven }
+
+(* quadrant *)
+type quad = I | II | III | IV
+type sign = Neg | Zero | Pos
+
+let sign (x:int) = 
+  if x < 0 then Neg
+  else if x = 0 then Zero
+  else Pos
+
+let quadrant : int*int -> quad option = fun(x,y) ->
+  match (sign x, sign y) with
+  | (Pos, Pos) -> Some I
+  | (Neg, Pos) -> Some II
+  | (Neg, Neg) -> Some III
+  | (Pos, Neg) -> Some IV
+  | _ -> None
+
+(* depth *) 
+type 'a tree = 
+| Leaf 
+| Node of 'a * 'a tree * 'a tree
+
+let rec depth tr = 
+  match tr with 
+  | Leaf -> 0
+  | Node (v, left, right) ->
+        let topDepth = (max (depth left) (depth right)) in 
+        1 + topDepth
+          
+(* list max *)
+let rec list_max (li: int list) = 
+  match li with 
+  | [] -> raise (Failure "list_max")
+  | [x] -> x
+  | x::xs -> let tempMax = list_max xs in 
+             if x > tempMax then x else tempMax
+
+let rec list_max_string (li: int list) = 
+  match li with 
+  | [] -> raise (Failure "list_max")
+  | [x] -> string_of_int x
+  | x::xs -> string_of_int (list_max li)
